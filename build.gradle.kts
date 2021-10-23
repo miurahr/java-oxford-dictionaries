32bef185854bb9d27cbde6f5cce4e6fff2e931f8
Hiroshi Miura <miurahr@linux.com>
2021-10-23 14:12:15 +0900

    First import

---
+++ build.gradle.kts
@@ -0,0 +1,77 @@
+plugins {
+    groovy
+    java
+    checkstyle
+    jacoco
+    id("com.github.spotbugs") version "4.7.9"
+    id("com.diffplug.spotless") version "5.17.0"
+    id("com.palantir.git-version") version "0.12.3"
+}
+
+// calculate version string from git tag, hash and commit distance
+fun getVersionDetails(): com.palantir.gradle.gitversion.VersionDetails = (extra["versionDetails"] as groovy.lang.Closure<*>)() as com.palantir.gradle.gitversion.VersionDetails
+if (getVersionDetails().isCleanTag) {
+    version = getVersionDetails().lastTag.substring(1)
+} else {
+    version = getVersionDetails().lastTag.substring(1) + "-" + getVersionDetails().commitDistance + "-" + getVersionDetails().gitHash + "-SNAPSHOT"
+}
+
+group = "tokyo.northside"
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.apache.httpcomponents.client5:httpclient5:5.1")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.13.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.codehaus.groovy:groovy-all:3.0.9")
+    testImplementation("commons-io:commons-io:2.11.0")
+}
+
+spotbugs {
+    excludeFilter.set(project.file("config/spotbugs/exclude.xml"))
+    tasks.spotbugsMain {
+        reports.create("html") {
+            isEnabled = true
+        }
+    }
+    tasks.spotbugsTest {
+        reports.create("html") {
+            isEnabled = true
+        }
+    }
+}
+
+tasks {
+    "jacocoTestReport"(JacocoReport::class) {
+        reports {
+            // To be read by humans
+            html.isEnabled = true
+            // To be read by Coveralls etc.
+            xml.isEnabled = true
+            xml.destination = file("$buildDir/reports/jacoco/test/jacocoTestReport.xml")
+        }
+    }
+
+    // Trying to run tests every time.
+    val test by tasks
+    val cleanTest by tasks
+    test.dependsOn(cleanTest)
+
+    // Use the built-in JUnit support of Gradle.
+    "test"(Test::class) {
+        useJUnitPlatform()
+    }
+
+    // Sorry, I have no idea.
+    Unit
+}
---
+++ changelog.md
@@ -0,0 +1,11 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+## [Unreleased]
+
+## [v0.1.0]
+
+* First alpha release.
+
+[Unreleased]: https://github.com/miurahr/java-oxford-dictionaries/compare/v0.1.0...HEAD
+[v0.1.0]: https://github.com/miurahr/java-oxford-dictionaries/compare/v0.0.1...v0.1.0
