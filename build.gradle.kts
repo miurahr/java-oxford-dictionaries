1d75763548dd7ae22404e1e981135bbed17bb7b8
Hiroshi Miura <miurahr@linux.com>
2021-12-20 17:39:03 +0900

    Bump Jackson@2.13.1
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,9 +39,9 @@ repositories {
 
 dependencies {
     implementation("org.apache.httpcomponents.client5:httpclient5:5.1.2")
-    implementation("com.fasterxml.jackson.core:jackson-core:2.13.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.0")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.13.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     testImplementation("org.codehaus.groovy:groovy-all:3.0.9")
